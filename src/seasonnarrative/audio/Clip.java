31ba4e37fa355176250b6cc6387b5d770b7f1b24
John Mooring <john@mooring.com>
2014-01-23 16:34:13 -0500

    Added base audio components

---
+++ src/seasonnarrative/audio/Clip.java
@@ -0,0 +1,96 @@
+package seasonnarrative.audio;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+/**
+ * Author: John Mooring (jmooring)
+ * Date: 10/12/13
+ * Time: 5:49 PM
+ */
+
+public class Clip{
+    private double[] audio;
+    private boolean playing = false;
+    private int loaded = 0;
+    private int location = 0;
+
+
+
+    public Clip(final String f) {
+        new Thread() {
+            public void run() {
+                AudioInputStream din = null;
+                try {
+                    File file = new File(f);
+                    AudioInputStream in = AudioSystem.getAudioInputStream(file);
+                    AudioFormat baseFormat = in.getFormat();
+                    AudioFormat decodedFormat = new AudioFormat(
+                            AudioFormat.Encoding.PCM_SIGNED,
+                            baseFormat.getSampleRate(), 16, baseFormat.getChannels(),
+                            baseFormat.getChannels() * 2, baseFormat.getSampleRate(),
+                            false);
+                    audio = new double[AudioSystem.getAudioFileFormat(file)
+                            .getByteLength() * 4];
+                    din = AudioSystem.getAudioInputStream(decodedFormat, in);
+
+                    byte[] data = new byte[4096];
+
+                    while (din.read(data, 0, data.length) != -1) {
+                        ByteBuffer bf = ByteBuffer.wrap(data);
+                        bf.order(ByteOrder.LITTLE_ENDIAN);
+
+                        while (bf.hasRemaining())
+                            audio[loaded++] = (bf.getShort() / ((double) Short.MAX_VALUE));
+                    }
+                    din.close();
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (din != null) {
+                        try {
+                            din.close();
+                        } catch (IOException ignored) {
+                        }
+                    }
+                }
+            }
+        }.start();
+    }
+
+    public void start() {
+        location = 0;
+        playing = true;
+    }
+
+    public void stop() {
+        playing = false;
+        location = 0;
+    }
+
+    public double read() {
+        if (playing) {
+            try {
+                while(location >= loaded){
+                    Thread.sleep(10);
+                }
+                if (location++ >= audio.length - 1) {
+                    stop();
+                    location = 0;
+                }
+                return audio[location];
+            } catch (Exception e) {
+                return 0;
+            }
+        } else
+            return 0;
+    }
+
+}
